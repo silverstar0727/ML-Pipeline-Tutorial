7e0943b16cac8048e9c601494d15cb32b0cd33fb
JeongMin Do <49096513+silverstar0727@users.noreply.github.com>
2021-07-01 17:14:19 +0900

    remove

--- kubeflow-pipeline/ex2-flipcoin/sequential-pipeline/build_image.sh
+++
@@ -1,1 +0,0 @@
-docker build -t silverstar456/kubeflow:1 ./dockerbuild
--- kubeflow-pipeline/ex2-flipcoin/sequential-pipeline/dockerbuild/flipcoin.py
+++
@@ -1,8 +0,0 @@
-import random
-
-if random.randint(0,1) == 0:
-    result = "heads" 
-else:
-    result = 'tails'
-
-print(result)
